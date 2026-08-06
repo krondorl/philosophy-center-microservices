027ac3cad227bc7fd71f37cc3249188388264acd
Adam Burucs <adam.burucs@gmail.com>
2026-08-06 19:07:40 +0200

    feat: modify app module

--- quote-service/src/app.module.ts
+++ quote-service/src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { QuotesModule } from './quotes/quotes.module';
+import { ConfigModule } from '@nestjs/config';
+import { resolve } from 'node:path';
 
 @Module({
-  imports: [HealthModule, QuotesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        resolve(__dirname, '..', '.secrets/quote-service.env'),
+        resolve(__dirname, '..', '.env'),
+      ],
+    }),
+    HealthModule,
+    QuotesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
