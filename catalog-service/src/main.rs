418734424514c7f6bc6d52b1f6769f7ae2a3221a
Adam Burucs <adam.burucs@gmail.com>
2026-08-31 16:54:34 +0200

    chore: add license to catalog service

--- catalog-service/src/main.rs
+++ catalog-service/src/main.rs
@@ -1,3 +1,11 @@
+/*!
+ * Philosophy Center Microservices
+ *
+ * Copyright (c) 2026- Adam Burucs
+ *
+ * MIT Licensed
+ */
+
 use axum::{Json, Router, extract::Path, extract::State, http::StatusCode, routing::get};
 use chrono::Utc;
 use dotenvy::dotenv;
