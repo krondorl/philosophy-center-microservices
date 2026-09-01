11fd0c99cbb3e20123c6125ec55f7c2ae282fd56
Adam Burucs <adam.burucs@gmail.com>
2026-09-01 12:27:10 +0200

    feat(quote service): add startup info

--- quote-service/src/main.ts
+++ quote-service/src/main.ts
@@ -6,11 +6,20 @@
  * MIT Licensed
  */
 
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3232);
+
+  const logger = new Logger();
+
+  logger.log('');
+  logger.log('Quote Service started');
+  logger.log(
+    `Server started at http://${process.env.SERVER_ADDRESS}:${process.env.SERVER_PORT}`,
+  );
 }
 bootstrap();
