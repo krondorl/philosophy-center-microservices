80521f8b2d2658f495f18e85ada6a3f2a5bf8452
Adam Burucs <adam.burucs@gmail.com>
2026-09-01 12:12:21 +0200

    feat(guide service): add startup info

--- guide-service/src/main/java/com/example/guide_service/GuideServiceApplication.java
+++ guide-service/src/main/java/com/example/guide_service/GuideServiceApplication.java
@@ -10,12 +10,29 @@ package com.example.guide_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GuideServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GuideServiceApplication.class, args);
-	}
+    @Value("${server.address:localhost}")
+    private String serverAddress;
 
+    @Value("${server.port:8080}")
+    private int serverPort;
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuideServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner printStartupMessage() {
+        return args -> {
+            System.out.println();
+            System.out.println("Guide Service started");
+            System.out.println("Server started at http://" + serverAddress + ":" + serverPort);
+        };
+    }
 }
